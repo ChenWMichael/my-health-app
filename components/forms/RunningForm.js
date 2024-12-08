58921feb918595d7d0e74876ea20921500d9cbbf
ChenWMichael <chenwm@seas.upenn.edu>
2024-12-07 23:57:36 -0800

    Added userState to RunningForm

--- components/forms/RunningForm.js
+++ components/forms/RunningForm.js
@@ -3,5 +3,12 @@ import { Box, TextField, Button } from '@mui/material';
 import { useState } from 'react';
 
 export default function RunningForm() {
-    
+    const [formData, setFormData] = userState({
+        distance: '',
+        time: '',
+        calories: '',
+        date: '',
+        notes: '',
+        }
+    )
 }
